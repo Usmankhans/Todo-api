0effaeb2ea4865f17e9ec671b1e392c03399c138
Usman Khan <usmankhan4619@gmail.com>
2017-03-04 14:51:41 +0200

    Delete method added

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ app.get('/', function(req, res){
 	res.send('Todo API Root');
 });
 
+//Get Todos
 app.get('/todos', function(req, res){
 	res.json(todos);
 });
 
+//Get Todos by Id
 app.get('/todos/:id', function(req, res){
 var todoId = parseInt(req.params.id,10);
 var found= _.findWhere(todos, {id: todoId});
@@ -27,6 +29,8 @@ else{
 	res.status(404).send();
 }
 });
+
+//Post Todos
 
 app.post('/todos', function (req,res){
 var body = _.pick(req.body, 'description', 'completed', 'id') ;
@@ -42,6 +46,23 @@ todos.push(body)
 res.json(body);
 
 });
+
+
+//Delete Todos by Id
+app.delete('/todos/:id', function(req, res){
+var todoId = parseInt(req.params.id,10);
+var found= _.findWhere(todos, {id: todoId});
+if(!found){
+	res.status(404).json({"error": "No todo item found for delete"});
+}
+else{
+	todos = _.without(todos, found);
+res.json(found);
+}
+
+});
+
+
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT + '!');
 });
